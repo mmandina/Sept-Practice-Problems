7a9ab01a7a194ea0f2ef76f32c87e43b48f89ae6
Max <maxwell.mandina@gmail.com>
2021-09-17 13:48:31 -0400

    map+subtract2

--- CSX/callbacks.js
+++ CSX/callbacks.js
@@ -9,3 +9,19 @@ function pluralize(array) {
 // Uncomment these to check your work!
 const animals = ["dog", "cat", "tree frog"];
 console.log(pluralize(animals));
+
+function subtractTwo(num) {
+  return num - 2;
+}
+function map(array, callback) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    newArray[i] = callback(array[i]);
+  }
+  return newArray;
+}
+
+// Uncomment these to check your work!
+console.log(typeof subtractTwo); // should log: 'function'
+console.log(typeof map); // should log: 'function'
+console.log(map([3, 4, 5], subtractTwo)); // should log: [ 1, 2, 3 ]
