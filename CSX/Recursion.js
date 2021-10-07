e2e228eed1857cfe35544468d2f3d688e2308d90
Max <maxwell.mandina@gmail.com>
2021-10-07 10:07:29 -0400

    Recursion finished

--- CSX/Recursion.js
+++ CSX/Recursion.js
@@ -27,3 +27,40 @@ function factorial(num) {
 //console.log(factorial(4)); // -> 24
 console.log(factorial(6)); // -> 720
 // console.log(factorial(0)); // -> 1
+function getLength(array, counter = 0) {
+  console.log(counter);
+  if (array[0] == undefined) {
+    return counter;
+  }
+  array.shift();
+
+  return getLength(array, ++counter);
+}
+
+// To check if you've completed the challenge, uncomment these console.logs!
+// console.log(getLength([1])); // -> 1
+console.log(getLength([1, 2])); // -> 2
+// console.log(getLength([1, 2, 3, 4, 5])); // -> 5
+// console.log(getLength([])); // -> 0
+function flow(input, funcArray) {
+  if (funcArray[0] == undefined) {
+    return input;
+  }
+  return flow(funcArray[0](input), funcArray.slice(1));
+}
+
+// To check if you've completed the challenge, uncomment this code!
+function multiplyBy2(num) {
+  return num * 2;
+}
+function add7(num) {
+  return num + 7;
+}
+function modulo4(num) {
+  return num % 4;
+}
+function subtract10(num) {
+  return num - 10;
+}
+const arrayOfFunctions = [multiplyBy2, add7, modulo4, subtract10];
+console.log(flow(2, arrayOfFunctions)); // -> -7
